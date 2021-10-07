ebdd1e3c6787275a55302d0b97cf1e2478d96153
Adrian Xhyra <adrianxhyra1995@gmail.com>
2021-10-07 15:59:32 +0200

    first push

---
+++ server.js
@@ -0,0 +1,11 @@
+const ronin     = require( 'ronin-server' )
+const mocks     = require( 'ronin-mocks' )
+const database  = require( 'ronin-database' )
+const server = ronin.server()
+
+database.connect( process.env.CONNECTIONSTRING )
+server.use( '/foo', (req, res) => {
+    console.log('adrian');
+    return res.json({ "foo": "bar" })
+})
+server.start()
